1f2fa1720f86e67d82fe49c3d77d0d6dc33c2878
Anil <ak26122k@gmail.com>
2023-09-28 21:15:53 +0530

    navbar and footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,25 @@
+
+"use client"
 import Image from "next/image"
 
+import  {CustomButtom}  from "@/components"
+
+
 const Hero = () => {
+
+    const handleScroll =()=>{}
   return (
     <div className="hero">
         <div className="flex-1 pt-36 padding-x">
             <h1 className="hero__title">Find, Book or rent a car --quickly and easily</h1>
             <p className="hero__subtitle"> Streamline your car rental experience with our effortless booking process.</p>
+           <CustomButtom title="Explore Cars"   containerStyle="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
+        </div>
+        <div className="hero__image-container">
+          <div className="hero__image">
+            <Image src="/hero.png" alt="hero"  fill className=" object-container"/>
+            <div className="hero__image-overlay"/>
+          </div>
         </div>
     </div>
   )
--- components/index.tsx
+++ components/index.tsx
@@ -1,5 +1,9 @@
+import CustomButtom from "./CustomButtom"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
 import Hero from "./Hero"
 
+
 export {
-    Hero
+    Hero,CustomButtom, Navbar, Footer
 }
